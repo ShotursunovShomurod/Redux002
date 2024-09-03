810455b24d86937ea8579d3b5229aa313104f56d
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-09-03 12:26:29 +0500

    togrilavomman1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import { useFetch } from "../../companents/hooks/useFetch";
 import Blogs from "../../companents/blogs/blogs";
+import App from "@/App.jsx";
 
 const Home = () => {
   const { data } = useFetch("/blogs", {}, []);
